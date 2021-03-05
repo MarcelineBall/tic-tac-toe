ee2c983ddb64672322c9c268ea40dfe702f93b17
Marceline Ball <marchettiball@gmail.com>
2021-03-05 10:35:25 -0700

    Create place piece method

--- game.js
+++ game.js
@@ -8,5 +8,14 @@ class Game {
     this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.playerTurn = 'one';
   }
+  placePiece(indexNumber) {
+    if (this.playerTurn === 'one') {
+      this.board[indexNumber] = 1;
+      this.playerTurn = 'two';
+    } else if (this.playerTurn === 'two') {
+      this.board[indexNumber] = 2;
+      this.playerTurn = 'one';
+    }
+  }
 }
 module.exports = Game
